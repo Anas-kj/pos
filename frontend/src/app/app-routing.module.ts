26f23ca2e3cbef4aaab6fd69705176f376caaeea
Anas-kj <anas.tadjibaeva@gmail.com>
2025-10-12 23:06:31 +0800

    Auth Fixed

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,10 +6,12 @@ import { LoginComponent } from './login/login.component';
 import { ForgetPasswordComponent } from './forget-password/forget-password.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
 import { ConfirmAccountComponent } from './confirm-account/confirm-account.component';
+import { canActivateRoute } from './services/guard/guard.service';
+import { Role } from 'src/models/interfaces/enums/role';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'employees', component: EmployeesComponent },
+  { path: 'employees', component: EmployeesComponent, canActivate: [canActivateRoute], data: {permittedRoles: [Role.Admin, Role.Superuser]} },
   { path: 'forgetPassword', component: ForgetPasswordComponent },
   { path: 'resetPassword', component: ResetPasswordComponent },
   { path: 'confirmAccount', component: ConfirmAccountComponent },
